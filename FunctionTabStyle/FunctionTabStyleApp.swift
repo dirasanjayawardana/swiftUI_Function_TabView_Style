22369828ef25d48f8262a28e0db605b01f67a7dc
Mona Lisa <andreno.juanda11@gmail.com>
2024-04-18 10:01:43 +0700

    Initial Commit

---
+++ FunctionTabStyle/FunctionTabStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunctionTabStyleApp.swift
+//  FunctionTabStyle
+//
+//  Created by MACBOOK PRO on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct FunctionTabStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
